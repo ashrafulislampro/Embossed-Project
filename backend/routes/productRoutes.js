c6a54da8ddfce5831a69bd90f7e69e8cf2fb1f9d
naimur <naimur202163@gmail.com>
2022-07-24 10:07:37 +0600

    product create route is created

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,6 +4,6 @@ const { getAllProducts, createProduct } = require("../controllers/productConroll
 const router = express.Router();
 
 router.route('/products').get(getAllProducts);
-router.route('/products/new').put(createProduct);
+router.route('/products/new').post(createProduct);
 
 module.exports = router;
