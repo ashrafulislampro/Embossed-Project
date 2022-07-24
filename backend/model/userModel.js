f742eefe24ff468671fd5be2533c6ac1e8dc2167
naimur <naimur202163@gmail.com>
2022-07-24 16:04:28 +0600

    userSchema added

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -2,28 +2,28 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please Enter Your Name"],
-    maxLength: [30, "Name can not exced 30 characters"],
-    minLength: [4, "Name Should have More than 4 characters"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+    minLength: [4, "Name should have more than 4 characters"],
   },
-
   email: {
     type: String,
     required: [true, "Please Enter Your Email"],
     unique: true,
-    validate: [validator.isEmail, "Please Enter a Valid Email"],
+    validate: [validator.isEmail, "Please Enter a valid Email"],
   },
   password: {
     type: String,
-    required: [true, "Please Enter Password"],
-    minLength: [8, "Pasword Should be greater than 8 characters"],
+    required: [true, "Please Enter Your Password"],
+    minLength: [8, "Password should be greater than 8 characters"],
     select: false,
   },
-  avater: {
+  avatar: {
     public_id: {
       type: String,
       required: true,
@@ -32,21 +32,19 @@ const userSchema = mongoose.Schema({
       type: String,
       required: true,
     },
-
-    role: {
-      type: String,
-      default: "user",
-    },
-    createdAt: {
-      type: Date,
-      default: Data.now(),
-    },
-
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
   },
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
-// Saving user
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
@@ -56,7 +54,7 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
 });
 
-// JWT token
+// JWT TOKEN
 userSchema.methods.getJWTToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
@@ -70,8 +68,17 @@ userSchema.methods.comparePassword = async function (password) {
 };
 
 // Generating Password Reset Token
-
 userSchema.methods.getResetPasswordToken = function () {
-  // Genreting Token
+  // Generating Token
   const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hashing and adding resetPasswordToken to userSchema
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+  return resetToken;
 };
