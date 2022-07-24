537dbb7f22789e7d1d4dd474f465db38b58cdf70
naimur <naimur202163@gmail.com>
2022-07-24 19:40:40 +0600

    shema added

--- backend/model/productModel.js
+++ backend/model/productModel.js
@@ -32,10 +32,7 @@ const productSchema = mongoose.Schema({
   },
   images: [
     {
-      // public_id: {
-      //   type: String,
-      //   required: true,
-      // },
+    
       url: {
         type: String,
         required: true,
