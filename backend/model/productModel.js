60e3ec6d6e82736051d2e1cba2fd0d3d757531a4
naimur <naimur202163@gmail.com>
2022-07-24 09:58:07 +0600

    product schema updated

--- backend/model/productModel.js
+++ backend/model/productModel.js
@@ -28,10 +28,10 @@ const productSchema = mongoose.Schema({
   },
   images: [
     {
-      public_id: {
-        type: String,
-        required: true,
-      },
+      // public_id: {
+      //   type: String,
+      //   required: true,
+      // },
       url: {
         type: String,
         required: true,
