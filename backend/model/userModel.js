7c20240ef1ceebaee63d3485c226bcf852e9c265
naimur <naimur202163@gmail.com>
2022-07-24 15:35:50 +0600

    user password schema added

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,10 +1,45 @@
-const mongoose=require('mongoose');
-const validator=require('validator');
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter Your Name"],
+    maxLength: [30, "Name can not exced 30 characters"],
+    minLength: [4, "Name Should have More than 4 characters"],
+  },
 
-const userSchema=mongoose.Schema({
-    type:String,
-    required:[true,"Please Enter Your Name"]
-})
+  email: {
+    type: String,
+    required: [true, "Please Enter Your Email"],
+    unique: true,
+    validate: [validator.isEmail, "Please Enter a Valid Email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please Enter Password"],
+    minLength: [8, "Pasword Should be greater than 8 characters"],
+    select: false,
+  },
+  avater: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      default: "user",
+    },
+    createdAt: {
+      type: Date,
+      default: Data.now(),
+    },
+  },
+});
