b6fb21a04194f7a3dc01308c39c16086b0116841
naimur <naimur202163@gmail.com>
2022-07-24 11:23:42 +0600

    update productDetails added

--- backend/controllers/productConroller.js
+++ backend/controllers/productConroller.js
@@ -1,51 +1,43 @@
+const cathcAsyncErrors = require("../middleware/cathcAsyncErrors");
 const Product = require("../model/productModel");
 const ErrorHandler = require("../utils/errorhander");
 
-
 // Geting all product
-exports.getAllProducts =  (req, res) => {
+exports.getAllProducts = cathcAsyncErrors(async (req, res, next) => {
   const product = await Product.find();
   res.status(200).json({
     success: true,
     message: "Product Got Successfully",
     product,
   });
-};
+});
 
 // Creating Product
-
-exports.createProduct = async (req, res, next) => {
-  try {
-    const product = await Product.create(req.body);
-
-    res.status(200).json({
-      success: true,
-      message: "product added",
-      product,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+exports.createProduct = cathcAsyncErrors(async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(200).json({
+    success: true,
+    message: "product added",
+    product,
+  });
+});
 
 // Geting Product Details
-exports.getProoductDetails = async (req, res, next) => {
-
-
-
+exports.getProoductDetails = cathcAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
-    return next(new ErrorHandler  ("Product not found", 404));
+    return next(new ErrorHandler("Product not found", 404));
   }
   res.status(200).json({
     success: true,
     product,
   });
-};
-
+});
 
 // Updating Product
-exports.updateProduct=async(req,res,next)=>{
-
-}
-
+exports.updateProduct =cathcAsyncErrors(async(req,res,next)=>{
+  res.status(200).json({
+    success:true,
+    message:"Hi am workin"
+  })
+})
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,13 @@ const {
   getAllProducts,
   createProduct,
   getProoductDetails,
+  updateProduct,
 } = require("../controllers/productConroller");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/products/new").post(createProduct);
-router.route("/products/:id").get(getProoductDetails);
+router.route("/products/:id").get(getProoductDetails).put(updateProduct);
 
 module.exports = router;
