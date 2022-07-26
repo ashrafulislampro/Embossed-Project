bf1912ef9cba4863feac6fe8c3a41a1b64003aa5
ashrafulislampro <ashrafulislam1659@gmail.com>
2022-07-26 13:17:28 +0600

    cors dependency code is added

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ const app = express();
 const connectDb = require("./config/dataBase");
 const products = require("./routes/productRoutes");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const users = require("./routes/userRoutes");
 // Midleware
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "/.env" });
 }
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 // Database Connection
